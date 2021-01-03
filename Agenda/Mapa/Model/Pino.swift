661462f2562ebf9e1a5e3f32a08f6f8531600a67
fabianasd <fafadiaz@hotmail.com>
2021-01-02 22:54:47 -0300

    Customizando pino no mapa

---
+++ Agenda/Mapa/Model/Pino.swift
@@ -0,0 +1,22 @@
+//
+//  Pino.swift
+//  Agenda
+//
+//  Created by Fabiana Petrovick on 02/01/21.
+//  Copyright © 2021 Fabiana Petrovick. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class Pino: NSObject {
+    
+    var title: String?
+    var icon: UIImage?
+    var color: UIColor?
+    var coordinate: CLLocationCoordinate2D
+    
+    init(coordenada:CLLocationCoordinate2D) {
+        self.coordinate = coordenada
+    }
+}
